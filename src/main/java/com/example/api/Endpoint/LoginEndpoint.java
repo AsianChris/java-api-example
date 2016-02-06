653ef9677e52aa20928083f5f1ecc12ec76a3dad
Chris Baptista <chris@asianchris.com>
2016-02-06 16:59:01 -0500

    Remove unnecessary import packages

--- src/main/java/com/example/api/Endpoint/LoginEndpoint.java
+++ src/main/java/com/example/api/Endpoint/LoginEndpoint.java
@@ -7,8 +7,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.FormParam;
-import javax.json.Json;
-import javax.json.JsonObject;
 
 import com.example.api.Model.JWT;
 import com.example.api.Model.User;
